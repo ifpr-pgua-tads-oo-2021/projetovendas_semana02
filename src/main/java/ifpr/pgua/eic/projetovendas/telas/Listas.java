426663372d0b260f450912395f61ce7f088562bf
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-12-14 08:08:34 -0300

    utilizando CellFactory

--- src/main/java/ifpr/pgua/eic/projetovendas/telas/Listas.java
+++ src/main/java/ifpr/pgua/eic/projetovendas/telas/Listas.java
@@ -4,6 +4,7 @@ import ifpr.pgua.eic.projetovendas.models.Pessoa;
 import ifpr.pgua.eic.projetovendas.models.Produto;
 import ifpr.pgua.eic.projetovendas.repositorios.RepositorioVendas;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
 public class Listas {
@@ -22,6 +23,19 @@ public class Listas {
     }
 
     public void initialize(){
+
+        lstPessoas.setCellFactory(lista -> new ListCell<>(){
+            protected void updateItem(Pessoa pessoa, boolean alterou) {
+                super.updateItem(pessoa, alterou);
+                if(pessoa != null){
+                    setText("("+pessoa.getId()+")"+pessoa.getNome());
+                }else{
+                    setText(null);
+                }
+            };
+        });
+
+
         lstPessoas.getItems().addAll(repositorio.listarPessoas());
         lstProdutos.getItems().addAll(repositorio.listarProdutos());
     }
